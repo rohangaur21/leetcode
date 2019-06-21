c153631269c3b42f67f58636ce38a63b08e6e071
Rohan Gaur <rohan.gaur@bhnetwork.com>
2019-06-21 01:01:54 -0500

    test

--- src/main/java/array/MedianInSortedArray.java
+++ src/main/java/array/MedianInSortedArray.java
@@ -1,6 +1,20 @@
 package array;
 
+import java.util.Arrays;
+
 public class MedianInSortedArray {
+
+    public double findMedianSortedArrays2(int[] nums1, int[] nums2) {
+        int tot = nums1.length + nums2.length;
+        int[] newArray = new int[nums1.length + nums2.length];
+
+        for (int i = 0; i < tot; i++) {
+            newArray[i] = i < nums1.length ? nums1[i] : nums2[i - nums1.length];
+        }
+        Arrays.sort(newArray);
+
+        return tot % 2 == 0 ? (newArray[(tot / 2) - 1] + newArray[(tot / 2)]) / 2.0 : newArray[(tot) / 2];
+    }
 
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int n1 = nums1.length;
@@ -15,12 +29,13 @@ public class MedianInSortedArray {
         } else {
             int[] nums = new int[n1 + n2];
             int i = 0, j = 0, k = 0;
-            while (i < n1 || j < n2) {
-                if (i < n1 && nums1[i] <= nums2[j]) {
-                    nums[k++] = nums1[i++];
+            while (k < (n1 + n2)) {
+                if (i < n1 && j < n2 && nums1[i] <= nums2[j]) {
+                    nums[k] = nums1[i++];
                 } else if (j < n2) {
-                    nums[k++] = nums2[j++];
+                    nums[k] = nums2[j++];
                 }
+                k++;
             }
             int n3 = nums.length;
             return (n3 % 2 > 0) ? nums[(n3 / 2)] : (nums[(n3 / 2) - 1] + nums[n3 / 2]) / 2.0;
@@ -28,8 +43,8 @@ public class MedianInSortedArray {
     }
 
     public static void main(String[] args) {
-        int[] arr1 = {1, 2, 3, 4};
-        int[] arr2 = {10};
+        int[] arr1 = {1, 2, 3, 20};
+        int[] arr2 = {7, 10};
         double d = new MedianInSortedArray().findMedianSortedArrays(arr1, arr2);
         System.out.println(d);
     }
