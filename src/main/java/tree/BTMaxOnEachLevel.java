5c5d3a3a8e02cf51624083766436f09e8e35ee58
rgaur00 <rohan.gaur@bhnetwork.com>
2019-10-11 23:35:38 -0500

    test

--- src/main/java/tree/BTMaxOnEachLevel.java
+++ src/main/java/tree/BTMaxOnEachLevel.java
@@ -1,33 +1,47 @@
 package tree;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class BTMaxOnEachLevel {
-}
+    public static void main(String[] args) {
+        BTMaxOnEachLevel opt = new BTMaxOnEachLevel();
+        BinaryTreeNode root = BinaryTreeNode.initBinaryTree(Arrays.asList(3, 5, 1, 6, 2, 0, 8, 5, 6, 7, 4));
+        System.out.println(opt.getMaxOnEachLevel(root));
+    }
 
 
+    public List<Integer> getMaxOnEachLevel(BinaryTreeNode root) {
+        if (root == null) return Collections.emptyList();
 
-    public List<Integer> getMaxOnEachLevel (BinaryTreeNode root){
-        if(root ==null ) return Collections.emptylist();
+
         List<Integer> results = new ArrayList<>();
         int max;
-
-        List <BinaryTreeNode> list = new ArrayList<BinaryTreeNode>();
-        List <BinaryTreeNode> newlist = new ArrayList<BinaryTreeNode>();
+        List<BinaryTreeNode> list = new ArrayList<BinaryTreeNode>();
+        List<BinaryTreeNode> newlist = new ArrayList<BinaryTreeNode>();
         newlist.add(root);
-        while(true){
+        while (true) {
             max = Integer.MIN_VALUE;
-            for(BinaryTreeNode n: list){
-                if(max<=n.data){
-                    max=n.data;
+            System.out.println(list);
+            for (BinaryTreeNode n : list) {
+                if (max <= n.data) {
+                    max = n.data;
                 }
-                if(node.right != null){
-                    newlist.add(node.right);
+                if (n.right != null) {
+                    newlist.add(n.right);
                 }
-                if(node.left !=null)
-                    newlist.add(node.left);
-            }}
-        results.add(max);
-        list=newlist;
-        newlist = new ArrayList<>;
+                if (n.left != null)
+                    newlist.add(n.left);
+            }
+            results.add(max);
+            if (newlist.isEmpty()) {
+                break;
+            }
+            list = newlist;
+            newlist = new ArrayList<>();
+        }
+        return results;
     }
 }
-return results;
