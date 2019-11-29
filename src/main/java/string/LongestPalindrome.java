41c5b292fd4ea439388bbf091fb3f0a31f355581
rohan gaur <rohan8721@gmail.com>
2019-11-29 11:21:12 -0600

    test

--- src/main/java/string/LongestPalindrome.java
+++ src/main/java/string/LongestPalindrome.java
@@ -1,34 +1,60 @@
-package string;
+    package string;
 
-public class LongestPalindrome {
-    public static String longestPalindrome(String s) {
-        String palin = "";
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = i; j < s.length(); j++) {
-                String sub = s.substring(i, j + 1);
-                if (isPalindrome(sub, 0, sub.length() - 1)) {
-                    if (palin.length() < sub.length()) {
-                        palin = sub;
+    public class LongestPalindrome {
+        public static String longestPalindrome(String s) {
+            String palin = "";
+            for (int i = 0; i < s.length(); i++) {
+                for (int j = i; j < s.length(); j++) {
+                    String sub = s.substring(i, j + 1);
+                    if (isPalindrome(sub, 0, sub.length() - 1)) {
+                        if (palin.length() < sub.length()) {
+                            palin = sub;
+                        }
                     }
                 }
             }
+            return palin;
         }
-        return palin;
-    }
 
-    public static boolean isPalindrome(String s, int i, int j) {
-        if (i == j) return true;
-        if (s.charAt(i) != s.charAt(j)) return false;
-        if (i < j) return isPalindrome(s, i + 1, j - 1);
-        return true;
-    }
+        public static boolean isPalindrome(String s, int i, int j) {
+            if (i == j) return true;
+            if (s.charAt(i) != s.charAt(j)) return false;
+            if (i < j) return isPalindrome(s, i + 1, j - 1);
+            return true;
+        }
 
-    public static void main(String[] args) {
-        System.out.println(longestPalindrome("babab"));
-        System.out.println(longestPalindrome(""));
-        System.out.println(longestPalindrome("bb"));
-        System.out.println(longestPalindrome("aaa"));
-        System.out.println(longestPalindrome("rohan"));
-        System.out.println(longestPalindrome("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"));
+        public static String longestPalindrome1(String s) {
+            if (s == null || s.length() < 1) return "";
+            int start = 0, end = 0;
+            for (int i = 0; i < s.length(); i++) {
+                int len1 = expandAroundCenter(s, i, i);
+                int len2 = expandAroundCenter(s, i, i + 1);
+                int len = Math.max(len1, len2);
+                if (len > end - start) {
+                    start = i - (len - 1) / 2;
+                    end = i + len / 2;
+                }
+            }
+            return s.substring(start, end + 1);
+        }
+
+        private static int expandAroundCenter(String s, int left, int right) {
+            int L = left, R = right;
+            while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+                L--;
+                R++;
+            }
+            int val = R - L - 1;
+            return val;
+        }
+
+
+        public static void main(String[] args) {
+            System.out.println(longestPalindrome1("babab"));
+            System.out.println(longestPalindrome(""));
+            System.out.println(longestPalindrome("bb"));
+            System.out.println(longestPalindrome("aaa"));
+            System.out.println(longestPalindrome("rohan"));
+            System.out.println(longestPalindrome("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"));
+        }
     }
-}
