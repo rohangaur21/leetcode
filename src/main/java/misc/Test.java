10dd9dab6145ec30ecc98d94691191558c909cf1
rohangaur <rohangaur21@bhnetwork.com>
2019-12-05 18:46:53 -0600

    test

--- src/main/java/misc/Test.java
+++ src/main/java/misc/Test.java
@@ -1,6 +1,6 @@
 package misc;
 
-import java.util.Date;
+import java.util.*;
 
 public class Test {
 
@@ -36,7 +36,45 @@ public class Test {
             System.out.println("Date1 is equal Date2");
         }
 
-    }
+
+        System.out.println("-------------------------");
+        int i = 010;
+        int j = 07;
+        System.out.println(i+""+j);
+
+        System.out.println("-------------------------");
+        int x=0;
+        int y=0;
+        for(int z = 0; z<5; z++){
+            if((++x > 2) && (++y > 2))
+                x++;
+        }
+        System.out.println(x+""+y);
+
+        System.out.println("-------------------------");
+        int l = 5;
+                int m=6;
+                String s1="7";
+        System.out.println(l+m+s1);
+
+
+        String [] c = {"Bangalore", "Pune","San Fr", "New Y"};
+        Arrays.sort(c, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o2.compareTo(o1);
+            }
+        });
+        System.out.println(Arrays.binarySearch(c, "New Y"));
+
+        int mm = 0x000F;
+        int vv = 0x2222;
+        /* System.out.printl /* n(vv&mm); *** */
+        System.out.println();
+
+//        new HashMap<>().get
+     }
+
 
 }
 
